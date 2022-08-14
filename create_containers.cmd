4f2dc9abe787bcdb83e20f341adc82fe25e9407a
Choonghyun Ryu <choonghyun.ryu@gmail.com>
2022-08-14 15:03:14 +0900

    chage from relative directory to absolute directory in create_container.sh

--- create_containers.cmd
+++ create_containers.cmd
@@ -1,1 +1,1 @@
-docker run -d -p 3838:3838 -p 8787:8787 -p 80:80 -e PASSWORD=rstudio -e ROOT=TRUE --name rstudio-shiny-server -v shiny-server/app:/srv/shiny-server/ -v web-server/html/:/var/www/html/ -v shiny-server/log:/var/log/shiny-server/ rserver:first 
+docker run -d -p 3838:3838 -p 8787:8787 -p 80:80 -e PASSWORD=rstudio -e ROOT=TRUE --name rstudio-shiny-server -v `pwd`/shiny-server/app:/srv/shiny-server/ -v `pwd`/web-server/html/:/var/www/html/ -v `pwd`/shiny-server/log:/var/log/shiny-server/ rserver:first 
